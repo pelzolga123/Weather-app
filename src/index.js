dd677aabed33a9bae80559fae672a2ffe5c8b6cc
pelzolga123 <pelzolga123@gmail.com>
2019-12-03 18:58:41 +0200

    Added working button for temperature conversion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-// const latitude = 51.507351;
-// const longtitude = -0.127758;
-// const key = process.env.DARKSKY_API_KEY;
 
 const getIcon = (currentIcon) => {
-  const icon = new Skycons({'color': 'orange'});
-  console.log(currentIcon);
+  // eslint-disable-next-line no-undef
+  const icon = new Skycons({ color: 'orange' });
   icon.set('icon', currentIcon);
   icon.play();
 };
@@ -15,16 +12,13 @@ const getWeather = (latitude, longtitude) => {
   const temperature = document.getElementById('result');
 
   fetch(fetchUrl)
-    .then(data => {
-      return data.json();
-    }).then(data => {
-      temperature.innerHTML = data.currently.temperature;
+    .then((data) => data.json()).then((data) => {
+      console.log(data.currently);
+      temperature.innerHTML = `${data.currently.temperature}&#8457;`;
       getIcon(data.currently.icon);
+      switchBtn(temperature, data.currently.temperature);
     })
-    .catch((err) => {
-      displayErr();
-      return err;
-    });
+    .catch((err) => err);
 
 };
 
@@ -32,26 +26,56 @@ const getCoordinates = (city) => {
   const fetchMap = `https://eu1.locationiq.com/v1/search.php?key=8345fc71b8f8b7&q=${city}&format=json`;
 
   fetch(fetchMap)
-    .then(data => {
-      return data.json();
-    }).then(data => {
+    .then((data) => data.json()).then((data) => {
       const latitude = data[0].lat;
       const longtitude = data[0].lon;
       getWeather(latitude, longtitude);
     })
-    .catch((err) => {
-      displayErr();
-      return err;
-    });
+    .catch((err) => err);
 };
 
-const getInput = () =>{
+function renderItem() {
+  fetch('https://source.unsplash.com/1600x1400/?beach').then((response) => {
+    document.body.setAttribute('style', `background-image:url("${response.url}")`);
+  });
+}
+
+renderItem();
+
+
+const getInput = () => {
   const getBtn = document.getElementById('getWeather');
   const request = document.getElementById('city');
 
   getBtn.addEventListener('click', (e) => {
     e.preventDefault();
     getCoordinates(request.value);
+  });
+};
+
+const countCelsius = (fahrenheit) => {
+  const celsius = Math.round((fahrenheit - 32) * (5 / 9));
+  return celsius;
+};
+
+const switchBtn = (header, value) => {
+  const btn = document.createElement('button');
+  const parentDiv = document.getElementById('res');
+  btn.innerHTML = '&#8451;';
+  btn.setAttribute('class', 'btn btn-primary');
+
+  parentDiv.appendChild(btn);
+
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (btn.innerHTML === '℃') {
+      btn.innerHTML = '&#8457;';
+      header.innerHTML = `${countCelsius(value)}&#8451;`;
+    } else if (btn.innerHTML === '℉') {
+      btn.innerHTML = '&#8451;';
+      header.innerHTML = `${value}&#8457;`;
+    }
   });
 };
 
